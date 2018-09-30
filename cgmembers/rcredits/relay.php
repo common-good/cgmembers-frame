2ec187712efd456d35477393eaad319597a43a4f
John Ridgway <john@jacelridge.com>
2018-09-30 11:29:02 -0400

    Remove files that are going to be in submodules

--- cgmembers/rcredits/relay.php
+++
@@ -1,5 +0,0 @@
-<?php
-$goto = urldecode(@$_GET['goto']);
-/// FAILS  if ($goto) header("Location: $goto", TRUE, 301);
-/**/ if ($goto) echo "<script>location.href='$goto';</script>"; else echo 'No goto!';
-
