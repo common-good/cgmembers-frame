6be1723b050bf55192c63805d6e4a659b2fdb32c
John Ridgway <john@jacelridge.com>
2018-09-30 17:46:57 -0400

    Allow it to deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-
+server "new.commongood.earth", roles: %w{app db web}
 
 
 # role-based syntax
@@ -30,7 +30,7 @@
 # For available Capistrano configuration variables see the documentation page.
 # http://capistranorb.com/documentation/getting-started/configuration/
 # Feel free to add new variables to customise your setup.
-
+set :branch, 'master'
 
 
 # Custom SSH Options
