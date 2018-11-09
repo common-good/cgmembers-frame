08cfb2eb18d9a9fb7108524cb8513ccea3eba1c2
John Ridgway <john@jacelridge.com>
2018-11-09 17:06:29 -0500

    deploy.rb specifies sharing config.json

--- config/deploy.rb
+++ config/deploy.rb
@@ -22,7 +22,7 @@ set :deploy_to, "/home/new/cgmembers-frame"
 
 # Default value for :linked_files is []
 # append :linked_files, "config/database.yml"
-append :linked_files, "cg.cnf"
+append :linked_files, "config/config.json"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
