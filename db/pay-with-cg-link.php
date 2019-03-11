72d41dd4a59c9bc4c150641b5a4a062cd40e9a2c
wspademan <wspademan@gmaiil.com>
2019-03-10 21:22:55 -0400

    2019-03-10  feature/pay-with-cg-link
    . people can now pay buy clicking on a simple link on a website or in an email -- no company code is specified and user must confirm by clickin a link in an email invoice (api and request parameters are stored in the invoice, so calling website can be notified upon payment)
    . added latitude and longitude fields
    . changed lookup restriction to number of miles
    . shortened company agreement
    . small changes to agreement text
    . fixed bug that suppressed letter choices for cAdmin2
    . fixed bug that suppressed formFollowup for admin2
    . changed periodic account risk thresholds to be a percentile of activity for that period length over all accounts
    . added solar, manages, lastTx, and snap fields in member list download (for marketing)
    . removed superfluous quotes in member list export
    . removed archaic decryption of doCodes
    . removed "invitation or card code" question from signup

---
+++ db/pay-with-cg-link.php
@@ -0,0 +1,21 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+class PayWithCgLink extends AbstractMigration {
+
+  public function change() {
+    $this->table('users')
+      ->addColumn('latitude', 'decimal', ['precision' => 11, 'scale'=>9, 'null' => false, 'default' => '0', 'comment' => 'latitude of account\'s physical address', 'after' => 'country']) 
+      ->addColumn('longitude', 'decimal', ['precision' => 11, 'scale'=>9, 'null' => false, 'default' => '0', 'comment' => 'longitude of account\'s physical address', 'after' => 'latitude']) 
+        ->update();
+    }
+    $this->table('r_stats')
+      ->addColumn('payees', 'decimal', ['precision' => 11, 'scale'=>2, 'null' => false, 'default' => '0', 'comment' => 'mean plus 10% of top quartile number of payees per active account over the past 30 days', 'after' => 'usdOutCount']) 
+      ->addColumn('basket', 'decimal', ['precision' => 11, 'scale'=>2, 'null' => false, 'default' => '0', 'comment' => 'mean (positive) amount per transaction over the past 30 days', 'after' => 'payees']) 
+      ->update();    
+  }
+
+}
