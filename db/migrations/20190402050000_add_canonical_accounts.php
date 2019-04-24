7029e1c8664b773083e7ec123ef520d7f83f794d
John Ridgway <jridgway@commongood.earth>
2019-04-24 14:18:41 -0400

    more fixes

--- db/migrations/20190402050000_add_canonical_accounts.php
+++ db/migrations/20190402050000_add_canonical_accounts.php
@@ -1,11 +1,9 @@
 <?php
 
-
 use Phinx\Migration\AbstractMigration;
 
-
-
 class AddCanonicalAccounts extends AbstractMigration {
+  
   const CG_ADMIN_UID = 1;  // uid of administrator account
   const PLACEHOLDER_1_UID = 2;  // uid of first placeholder
   const PLACEHOLDER_2_UID = 3;  // uid of second placeholder
@@ -16,21 +14,22 @@ class AddCanonicalAccounts extends AbstractMigration {
   const CG_OUTGOING_BANK_UID = 257;  //
 
   public function up() {
+    $this->execute("DELETE FROM users where uid IN (" . self::CG_ROUNDUPS_UID . ", " . self::CG_CRUMBS_UID . ", " . self::CG_SERVICE_CHARGES_UID . ", " . self::CG_INCOMING_BANK_UID . ", " . self::CG_OUTGOING_BANK_UID . ")");
     $this->table('users')
-      ->insert(['uid'=>CG_ROUNDUPS_UID, 'community'=>0, 'name'=>'roundups', 'fullName'=>'Roundup Donations',
+      ->insert(['uid'=>self::CG_ROUNDUPS_UID, 'community'=>0, 'name'=>'roundups', 'fullName'=>'Roundup Donations',
                 'email'=>'cg@commongood.earth', 'zip'=>'', 'country'=>0, 'minimum'=>0, 'flags'=>6])
-      ->insert(['uid'=>CG_CRUMBS_UID, 'community'=>0, 'name'=>'crumbs', 'fullName'=>'Crumb Donations',
+      ->insert(['uid'=>self::CG_CRUMBS_UID, 'community'=>0, 'name'=>'crumbs', 'fullName'=>'Crumb Donations',
                 'email'=>'cg@commongood.earth', 'zip'=>'', 'country'=>0, 'minimum'=>0, 'flags'=>6])
-      ->insert(['uid'=>CG_SERVICE_CHARGES_UID, 'community'=>0, 'name'=>'service charges', 'fullName'=>'Service Charges',
+      ->insert(['uid'=>self::CG_SERVICE_CHARGES_UID, 'community'=>0, 'name'=>'service charges', 'fullName'=>'Service Charges',
                 'email'=>'cg@commongood.earth', 'zip'=>'', 'country'=>0, 'minimum'=>0, 'flags'=>6])
-      ->insert(['uid'=>CG_INCOMING_BANK_UID, 'community'=>0, 'name'=>'bank-in', 'fullName'=>'--',
+      ->insert(['uid'=>self::CG_INCOMING_BANK_UID, 'community'=>0, 'name'=>'bank-in', 'fullName'=>'--',
                 'email'=>'cg@commongood.earth', 'zip'=>'', 'country'=>0, 'minimum'=>0, 'flags'=>6]) 
-      ->insert(['uid'=>CG_OUTGOING_BANK_UID, 'community'=>0, 'name'=>'bank-out', 'fullName'=>'--',
+      ->insert(['uid'=>self::CG_OUTGOING_BANK_UID, 'community'=>0, 'name'=>'bank-out', 'fullName'=>'--',
                 'email'=>'cg@commongood.earth', 'zip'=>'', 'country'=>0, 'minimum'=>0, 'flags'=>6])
       ->save();
   }
 
   public function down() {
-    $this->execute("DELETE FROM users where uid IN (" . CG_ROUNDUPS_UID . ", " . CG_CRUMBS_UID . ", " . CG_SERVICE_CHARGES_UID . ", " . CG_INCOMING_BANK_UID . ", " . CG_OUTGOING_BANK_UID . ")");
+    $this->execute("DELETE FROM users where uid IN (" . self::CG_ROUNDUPS_UID . ", " . self::CG_CRUMBS_UID . ", " . self::CG_SERVICE_CHARGES_UID . ", " . self::CG_INCOMING_BANK_UID . ", " . self::CG_OUTGOING_BANK_UID . ")");
   }
 }
