2cee798cdf3b491306069793c5c6a8ac1258a997
wspademan <wspademan@gmaiil.com>
2019-06-03 17:56:16 -0400

    2019-06-03 tweaks to feature/fix-balance-sheet
    . changed TM to &reg; on CG cards
    . signup now records source2 sent from promo site as advertisement ID
    . community is no longer automatically updated to/from seedpack for ever-activate members
    . email now goes through google's SMTP

--- vendor/bin/phinx.bat
+++ vendor/bin/phinx.bat
@@ -1,2 +1,2 @@
 REM Created by wspademan 5/30/2019
-vendor/robmorgan/phinx/bin/phinx "%1" -c config/phinx.json "%2"
+vendor/robmorgan/phinx/bin/phinx "%1" -c config/phinx.json "%2" %3 %4 %5 %6 %7 %8
