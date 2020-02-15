60baddde889383acc4040e7ab077565ab6d3f930
wspademan <wspademan@gmail.com>
2020-02-15 13:12:16 -0500

    2020-02-15 hotfix/task-nudge
    . whatever task is current, send member to next step
    . final tweak to solar1 activation
    . enable deployment to new server (port change)
    . allow deletion of company account with discount

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -48,7 +48,8 @@ set :ssh_options, {
   # keys: %w(/home/rlisowski/.ssh/id_rsa),
   forward_agent: false,
   auth_methods: %w(publickey),
-  user: "new"
+  user: "new",
+  port: 7822  
 }
 #
 # The server-based syntax can be used to override options:
