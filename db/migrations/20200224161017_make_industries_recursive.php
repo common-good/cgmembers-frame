7d7bdbabc15d01b0e79e075d5d7fc70efae62a67
wspademan <wspademan@gmail.com>
2020-02-25 15:26:13 -0500

    minor tweaks to treed industries

--- db/migrations/20200224161017_make_industries_recursive.php
+++ db/migrations/20200224161017_make_industries_recursive.php
@@ -13,18 +13,18 @@ class MakeIndustriesRecursive extends AbstractMigration
     $n = $this->execute('update r_industries set parent = null where parent = iid');
     $this->execute('CREATE VIEW descendants AS
       WITH RECURSIVE descendants AS (
-        SELECT iid AS base, industry AS base_industry, iid, industry FROM r_industries
+        SELECT iid AS base, industry AS baseIndustry, iid, industry FROM r_industries
         UNION ALL
-        SELECT d.base AS base, d.base_industry AS base_industry, c.iid, c.industry 
+        SELECT d.base AS base, d.baseIndustry AS baseIndustry, c.iid, c.industry 
         FROM r_industries c INNER JOIN descendants d ON d.iid = c.parent )
-      SELECT base, base_industry, iid AS descendant, industry AS descendant_industry FROM descendants');
+      SELECT base, baseIndustry, iid AS descendant, industry AS descendantIndustry FROM descendants');
     $this->execute('CREATE VIEW ancestors AS
       WITH RECURSIVE ancestors AS (
-        SELECT iid AS base, industry AS base_industry, iid, industry, parent FROM r_industries
+        SELECT iid AS base, industry AS baseIndustry, iid, industry, parent FROM r_industries
         UNION ALL
-        SELECT a.base AS base, a.base_industry AS base_industry, p.iid, p.industry, p.parent
+        SELECT a.base AS base, a.baseIndustry AS baseIndustry, p.iid, p.industry, p.parent
         FROM r_industries p INNER JOIN ancestors a ON p.iid = a.parent )
-      SELECT base, base_industry, iid AS ancestor, industry AS ancestor_industry FROM ancestors');
+      SELECT base, baseIndustry, iid AS ancestor, industry AS ancestorIndustry FROM ancestors');
   }
 
   public function down() {
