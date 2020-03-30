cc7c1ebec46be1dd4a9b86c71cf82b87030bdd7e
John Ridgway <jridgway@commongood.earth>
2020-03-30 16:16:41 -0400

    remove the echo from the command lines

--- lib/capistrano/restarter/restarter.rb
+++ lib/capistrano/restarter/restarter.rb
@@ -2,13 +2,13 @@ namespace :restarter do
   desc 'Restarts nginx'
   task :restart_nginx do
     on roles(:app) do
-      execute 'echo sudo /bin/systemctl restart nginx'
+      execute 'sudo /bin/systemctl restart nginx'
     end
   end
   desc 'Restarts php-fpm'
   task :restart_php_fpm do
     on roles(:app) do
-      execute 'echo sudo /bin/systemctl restart php-fpm'
+      execute 'sudo /bin/systemctl restart php-fpm'
     end
   end
 end
