227e71a552db26cbaaee7dc75e9cb4b76b77eca1
wspademan <wspademan@gmail.com>
2020-09-03 08:33:17 -0400

    2020-09-03 feature/post-notices
    . users can set preferences for frequency of notices about posts

--- db/migrations/20200507233810_post_notices.php
+++ db/migrations/20200507233810_post_notices.php
@@ -4,12 +4,15 @@ use Phinx\Migration\AbstractMigration;
 use Phinx\Db\Adapter\MysqlAdapter as phx;
 
 require_once __DIR__ . '/util.inc';
+const NOTICE_DFTS = 'offer:d,need:d,tip:w,request:w,auto:d,othertx:i,other:d';
 
 class PostNotices extends AbstractMigration {
 
   public function change() {
-    $t = $this-table('people');
-    $t->addColumn('notices', 'string', ray('length null comment', phx::TEXT_MEDIUM, TRUE, 'post notice preferences'));
+    $t = $this->table('people');
+    $t->addColumn('notices', 'text', ray('length null comment after', phx::TEXT_TINY, TRUE, 'notice preferences', 'created'));
     $t->save();
+    $this->execute("UPDATE people SET notices='" . NOTICE_DFTS . "'");
+    $this->execute("UPDATE users SET notices='" . NOTICE_DFTS . "'");
   }
 }
