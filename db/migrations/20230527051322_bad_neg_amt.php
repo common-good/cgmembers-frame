2fe906fd3cdd925b4f9d105f27d1fc555aeb7903
William Spademan <wspademan@CommonGood.earth>
2023-05-27 13:19:13 -0400

    fix oversight in bad-neg-amt feature

--- db/migrations/20230527051322_bad_neg_amt.php
+++ db/migrations/20230527051322_bad_neg_amt.php
@@ -7,7 +7,7 @@ require_once __DIR__ . '/util.inc';
 
 class BadNegAmt extends AbstractMigration {
   public function up() {
-    doSql("ALTER TABLE `tx_bads` CHANGE `amount` `amount` DECIMAL(11,2) NOT NULL DEFAULT '0.00' COMMENT 'amount to pay or charge';");
+    $this->doSql("ALTER TABLE `tx_bads` CHANGE `amount` `amount` DECIMAL(11,2) NOT NULL DEFAULT '0.00' COMMENT 'amount to pay or charge';");
   }
   public function doSql($sql) {cgpr("$sql\n"); $this->execute($sql);}
 }
