84aab7c89e27c8a49f32f0c81cc5e0da251ca840
William Spademan <wspademan@CommonGood.earth>
2023-09-15 11:51:07 -0400

    fix thermometer data fields

--- db/migrations/20230811023357_thermometer.php
+++ db/migrations/20230811023357_thermometer.php
@@ -7,10 +7,12 @@ require_once __DIR__ . '/util.inc';
 
 class Thermometer extends AbstractMigration {
   public function change() {
-    $t = $this->table('u_company');
-    $t->addColumn('target', 'decimal', ray('precision scale null default comment after', 11, 2, FALSE, '0', 'fundraising target amount', 'logo'));
-    $t->addColumn('targetStart', 'integer', ray('length null comment after', 11, TRUE, 'starting date of fundraising project', 'target'));
-    $t->update();
+    foreach (ray('u_company x_company') as $tnm) {
+      $t = $this->table($tnm);
+      $t->addColumn('target', 'decimal', ray('precision scale null default comment after', 11, 2, FALSE, '0', 'fundraising target amount', 'logo'));
+      $t->addColumn('targetStart', 'integer', ray('length null comment after', 11, TRUE, 'starting date of fundraising project', 'target'));
+      $t->update();
+    }
   }
 }
 
