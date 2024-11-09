27384a95e2a6d6f94bcc5cc5afc2f6372a8a7a09
William Spademan <wspademan@gmail.com>
2024-11-09 12:50:12 -0500

    bug fix in Nicks migration

--- db/migrations/20241109015357_nicks.php
+++ db/migrations/20241109015357_nicks.php
@@ -8,9 +8,9 @@ require_once __DIR__ . '/util.inc';
 class Nicks extends AbstractMigration {
   public function change() {
     if ($this->isMigratingUp()) {
-      $this->doSql("UPDATE tx_cats SET nick='CG-GRANT-PERSON', WHERE id=5050");
-      $this->doSql("UPDATE tx_cats SET nick='CG-GRANT-ORG', WHERE id=5100");
-      $this->doSql("UPDATE tx_cats SET nick='CG-GRANT-FOREIGN', WHERE id=5150");
+      $this->doSql("UPDATE tx_cats SET nick='CG-GRANT-PERSON' WHERE id=5050");
+      $this->doSql("UPDATE tx_cats SET nick='CG-GRANT-ORG' WHERE id=5100");
+      $this->doSql("UPDATE tx_cats SET nick='CG-GRANT-FOREIGN' WHERE id=5150");
       $this->doSql("UPDATE tx_cats SET nick='GRANT-PERSON' WHERE id=8900");
       $this->doSql("UPDATE tx_cats SET nick='GRANT-ORG' WHERE id=8905");
       $this->doSql("UPDATE tx_cats SET nick='GRANT-FOREIGN' WHERE id=8910");
