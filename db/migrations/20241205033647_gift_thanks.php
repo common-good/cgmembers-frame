c1125b2797ef6f4f8741ed757f5055f3e4ef6843
William Spademan <wspademan@gmail.com>
2024-12-05 11:38:58 -0500

    allow companies to specify an extra line in donation thanks emails

---
+++ db/migrations/20241205033647_gift_thanks.php
@@ -0,0 +1,16 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter as phx;
+
+require_once __DIR__ . '/util.inc';
+
+class GiftThanks extends AbstractMigration {
+  public function change() {
+    $t = $this->table('u_company');
+    $t->addColumn('giftThanks', 'text', ray('length null after comment', phx::TEXT_LONG, TRUE, 'staleNudge', 'extra email text for donation thanks'));
+    $t->update();
+    if ($this->isMigratingUp()) {}
+  }
+//  public function doSql($sql) {cgpr("$sql\n"); $this->execute($sql);}
+}
